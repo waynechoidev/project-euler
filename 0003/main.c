bfb0653aa26b5ddd4c3e6dd6a994fa056515561e
waynechoidev <wonjun92@gmail.com>
2025-08-07 14:44:34 +1200

    0003

---
+++ 0003/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    long long n = 600851475143;
+    long long largest = 0;
+
+    for (long long i = 2; i * i <= n; ++i)
+    {
+        while (n % i == 0)
+        {
+            largest = i;
+            n /= i;
+        }
+    }
+
+    if (n > 1)
+        largest = n;
+
+    printf("%lld\n", largest);
+}
