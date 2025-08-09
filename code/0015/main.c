2d3261ccdcb7038bd7b2af08cbb517497caef2e4
waynechoidev <wonjun92@gmail.com>
2025-08-09 23:42:09 +1200

    0015

---
+++ code/0015/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define SIZE 20
+
+int main()
+{
+    long long dp[SIZE + 1][SIZE + 1] = {0};
+    for (int i = 0; i <= SIZE; ++i)
+        dp[i][0] = dp[0][i] = 1;
+
+    for (int i = 1; i <= SIZE; ++i)
+        for (int j = 1; j <= SIZE; ++j)
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+
+    printf("%lld", dp[SIZE][SIZE]);
+
+    return 0;
+}
